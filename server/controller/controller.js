07df4912098061ce344b3bf3e410bac8f73b352b
Pierre Sandy <syndaphex@outlook.com>
2022-03-18 08:42:41 +1300

    first commit

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -12,6 +12,7 @@ exports.create = (req,res)=>{
     const user = new Userdb({
         name : req.body.name,
         email : req.body.email,
+        age : req.body.age,
         gender: req.body.gender,
         status : req.body.status
     })
@@ -94,7 +95,7 @@ exports.delete = (req, res)=>{
                 res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
             }else{
                 res.send({
-                    message : "User was deleted successfully!"
+                    message : "User was deleted from Database."
                 })
             }
         })
